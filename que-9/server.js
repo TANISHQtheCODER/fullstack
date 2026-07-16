bbf832e9f1a35e25ce80c3f46b6acd5e6d5cfcae
TANISHQtheCODER <2023040358@vnsgu.ac.in>
2026-07-16 11:44:16 +0530

    Update server.js

--- que-9/server.js
+++ que-9/server.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-// 1. Create a file
 fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
     if (err) {
         console.log(err);
@@ -9,7 +8,6 @@ fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
 
     console.log("File created successfully.");
 
-    // 2. Append data
     fs.appendFile("sample.txt", "\nWelcome to FS Module.", (err) => {
         if (err) {
             console.log(err);
@@ -18,7 +16,6 @@ fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
 
         console.log("Data appended successfully.");
 
-        // 3. Read file
         fs.readFile("sample.txt", "utf8", (err, data) => {
             if (err) {
                 console.log(err);
@@ -28,7 +25,6 @@ fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
             console.log("\nFile Content:");
             console.log(data);
 
-            // 4. Rename file
             fs.rename("sample.txt", "newSample.txt", (err) => {
                 if (err) {
                     console.log(err);
@@ -37,7 +33,6 @@ fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
 
                 console.log("\nFile renamed successfully.");
 
-                // 5. Delete file
                 fs.unlink("newSample.txt", (err) => {
                     if (err) {
                         console.log(err);
