6b7b8292c7b6cd601661c6ebd655b9d2915c5514
TANISHQtheCODER <2023040358@vnsgu.ac.in>
2026-07-16 11:43:28 +0530

    Update delete.js

--- que-6/delete.js
+++ que-6/delete.js
@@ -1,10 +1,8 @@
 const fs = require("fs");
 const util = require("util");
 
-// Convert fs.unlink into Promise-based function
 const unlinkFile = util.promisify(fs.unlink);
 
-// Delete the file
 unlinkFile("test.txt")
     .then(() => {
         console.log("File deleted successfully.");
